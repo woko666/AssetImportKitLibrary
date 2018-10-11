c1c9bf820f7195a4171b79df14139e5163552731
woko <woko@macbook-8.local>
2018-10-11 17:58:39 +0200

    Initial commit

---
+++ AssetImportKitLibrary/AssetImportKitLibrary.swift
@@ -0,0 +1,11 @@
+//
+//  AssetImportKitLibrary.swift
+//  AssetImportKitLibrary
+//
+//  Created by woko on 11/10/2018.
+//  Copyright © 2018 Woko. All rights reserved.
+//
+
+class AssetImportKitLibrary {
+
+}
